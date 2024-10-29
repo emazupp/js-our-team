b2254a461de17a209234d67bb996df14290e5fe8
emazupp <zuppardo.emanuele2001@gmail.com>
2024-10-29 15:05:03 +0100

    created function for generate cards

--- js/script.js
+++ js/script.js
@@ -51,22 +51,28 @@ let card = `
   </div>
 </div>`;
 
+const cardsContainerHtml = document.getElementById("cards-container");
+
 const getElementFromArray = () => {
   let cardsHtml = ``;
-  teamMembers.forEach((member) => {});
-  const fullName = member.name.toUpperCase();
-  let card = `
-  <div class="col-4">
+  teamMembers.forEach((member) => {
+    const fullName = member.name.toUpperCase();
+    let card = `
+    <div class="col-4">
     <div class="d-flex text-bg-dark">
-      <figure class="m-0 pe-2">
-        <img class="img-fluid" src=${member.img} alt="" />
-      </figure>
-      <div class="d-flex flex-column justify-content-center">
-        <span class="d-flex">${fullName}</span>
-        <span class="d-flex">${member.role}</span>
-        <span class="d-flex">${member.email}</span>
-      </div>
+    <figure class="m-0 pe-2">
+    <img class="img-fluid" src=${member.img} alt="" />
+    </figure>
+    <div class="d-flex flex-column justify-content-center">
+    <span class="d-flex fw-bold">${fullName}</span>
+    <span class="d-flex role">${member.role}</span>
+    <span class="d-flex"><a href="mailto:${member.email}">${member.email}</a></span>
     </div>
-  </div>`;
-  cardsHtml += card;
+    </div>
+    </div>`;
+    cardsHtml += card;
+  });
+  cardsContainerHtml.innerHTML = cardsHtml;
 };
+
+getElementFromArray();
